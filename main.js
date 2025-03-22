2f236cc5157c89154a8f9c47565334db7876aa7d
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-21 20:42:14 -0500

    Practice Arrays One

---
+++ main.js
@@ -0,0 +1,11 @@
+let groceryList = ["Milk", "Eggs", "Cheese", "Rice", "Chicken"];
+console.log(groceryList);
+
+let priceListOne = [5.00, 3.00, 4.00];
+console.log(priceListOne);
+
+let homeProjectlist = ["hammer", "nails", "screws"]
+console.log(homeProjectlist);
+
+let priceListTwo = [8.00, 9.00]
+console.log(priceListTwo);
